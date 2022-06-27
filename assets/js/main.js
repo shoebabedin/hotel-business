b0a72894ae2f4b94b508d3208534120d58f2f415
shoebabedin <shoeb.abedin@intelli.global>
2022-06-27 16:14:44 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -471,11 +471,14 @@ $(document).ready(function () {
          : ""
      );
    };
+
    $("#caleran-ex-6-5-start").caleran({
      startEmpty: $("#caleran-ex-6-5-start").val() === "",
      startDate: $("#caleran-ex-6-5-start").val(),
      endDate: $("#caleran-ex-6-5-end").val(),
      enableKeyboard: false,
+
+  
      oninit: function (instance) {
        startInstance = instance;
        if (!instance.config.startEmpty && instance.config.startDate) {
@@ -561,5 +564,48 @@ $(document).ready(function () {
        endInstance.globals.endSelected = false;
      },
    });
+
+ 
    // date picker
+  //  document.getElementsByClassName('fromdate').value = new Date();
+   $(".fromdate").datepicker({
+    dateFormat: "yy-mm-dd",
+    changeMonth: true,
+    changeYear: true,
+  });
+  $(".todate").datepicker({
+    dateFormat: "yy-mm-dd",
+    changeMonth: true,
+    changeYear: true,
+  });
+  $(".fromdate")
+    .datepicker()
+    .bind("change", function () {
+      var minValue = $(this).val();
+      minValue = $.datepicker.parseDate("yy-mm-dd", minValue);
+      $(".todate").datepicker("option", "minDate", minValue);
+      calculate();
+    });
+  $(".todate")
+    .datepicker()
+    .bind("change", function () {
+      var maxValue = $(this).val();
+      maxValue = $.datepicker.parseDate("yy-mm-dd", maxValue);
+      $(".fromdate").datepicker("option", "maxDate", maxValue);
+      calculate();
+    });
+
+  function calculate() {
+    var d1 = $(".fromdate").datepicker("getDate");
+    var d2 = $(".todate").datepicker("getDate");
+    var oneDay = 24 * 60 * 60 * 1000;
+    var diff = 0;
+    if (d1 && d2) {
+      diff = Math.round(Math.abs((d2.getTime() - d1.getTime()) / oneDay));
+    }
+    $(".calculated").val(diff);
+  } 
 });
+
+
+
